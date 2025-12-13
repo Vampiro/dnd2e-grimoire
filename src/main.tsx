267786034c52f8fe129e9bab88052d32a9be4783
Robert Auld <sayhitorobbie@gmail.com>
2025-12-12 23:53:13 -0500

    Setting base in react router.

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
+const base = import.meta.env.VITE_BASE_URL.replace(/\/$/, "");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
   </StrictMode>,
